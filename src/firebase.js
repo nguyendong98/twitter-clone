1f974dd038a91baf32b980ce0671b62470e4af8a
hhthien <hhthien@tma.com.vn>
2021-06-06 16:37:35 +0700

    initialization firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrE1WaL3aVA6xtx3wJjrf0PfROdKt5zTc",
+    authDomain: "twitter-clone-59398.firebaseapp.com",
+    projectId: "twitter-clone-59398",
+    storageBucket: "twitter-clone-59398.appspot.com",
+    messagingSenderId: "358594408813",
+    appId: "1:358594408813:web:eae6f27669413744656a64"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);

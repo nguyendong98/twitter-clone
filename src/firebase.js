3622aa305d6a3edc5abb9238f4094d68d5778477
hhthien <hhthien@tma.com.vn>
2021-06-06 16:51:40 +0700

    set up .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrE1WaL3aVA6xtx3wJjrf0PfROdKt5zTc",
-    authDomain: "twitter-clone-59398.firebaseapp.com",
-    projectId: "twitter-clone-59398",
-    storageBucket: "twitter-clone-59398.appspot.com",
-    messagingSenderId: "358594408813",
-    appId: "1:358594408813:web:eae6f27669413744656a64"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
